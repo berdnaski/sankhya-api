a62c62b5e5ca7dc4caf01ba5381c9aecd439307f
berdnaski <berdnaski1177@gmail.com>
2024-09-05 12:11:19 -0300

    feat: delete, update and list, customers.

--- src/main/java/github/com/berdnaski/sankhya_api/rest/controllers/CustomerController.java
+++ src/main/java/github/com/berdnaski/sankhya_api/rest/controllers/CustomerController.java
@@ -5,9 +5,12 @@ import github.com.berdnaski.sankhya_api.rest.dto.CreateCustomerDTO;
 import github.com.berdnaski.sankhya_api.rest.dto.ListCustomerDTO;
 import github.com.berdnaski.sankhya_api.service.CustomerService;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/customers")
@@ -23,12 +26,11 @@ public class CustomerController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Customer> getCustomerById(@PathVariable String id) {
-        var customer = customerService.getCustomerById(id);
-
-        if(customer.isPresent()) {
-            return ResponseEntity.ok(customer.get());
-        } else {
-            return ResponseEntity.notFound().build();
+        try {
+            Customer customer = customerService.getCustomerById(id);
+            return ResponseEntity.ok(customer);
+        } catch (RuntimeException e ) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
     }
 
@@ -36,10 +38,28 @@ public class CustomerController {
     public ResponseEntity<ListCustomerDTO> getCustomerByPhone(@PathVariable String phone) {
         var customer = customerService.findByPhone(phone);
 
-        if(customer.isPresent()) {
+        if (customer.isPresent()) {
             return ResponseEntity.ok(customer.get());
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ListCustomerDTO>> listAll() {
+        List<ListCustomerDTO> customers = customerService.listAll();
+        return ResponseEntity.ok(customers);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@PathVariable UUID id, @RequestBody CreateCustomerDTO updateCustomerDTO) {
+        customerService.updateCustomerById(id, updateCustomerDTO);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable String id) {
+        customerService.deleteCustomerById(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/github/com/berdnaski/sankhya_api/service/CustomerService.java
+++ src/main/java/github/com/berdnaski/sankhya_api/service/CustomerService.java
@@ -6,8 +6,12 @@ import github.com.berdnaski.sankhya_api.rest.dto.CreateCustomerDTO;
 import github.com.berdnaski.sankhya_api.rest.dto.ListCustomerDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -28,13 +32,60 @@ public class CustomerService {
         return customerRepository.save(customer);
     }
 
-    public Optional<Customer> getCustomerById(String id) {
-        return customerRepository.findById(UUID.fromString(id));
+    public Customer getCustomerById(String customerId) {
+        return customerRepository.findById(UUID.fromString(customerId)).orElseThrow(() -> new RuntimeException("Customer id not found: " + customerId));
     }
+
 
     public Optional<ListCustomerDTO> findByPhone(String phone) {
         return customerRepository
                     .findByPhone(phone)
                 .map(customer -> new ListCustomerDTO(customer.getName(), customer.getPhone()));
+    }
+
+    public List<ListCustomerDTO> listAll() {
+        return customerRepository.findAll().stream()
+                .map(customer -> new ListCustomerDTO(
+                        customer.getName(),
+                        customer.getPhone()
+                )).collect(Collectors.toList());
+    }
+
+    public void updateCustomerById(UUID customerId, CreateCustomerDTO updateCustomerDTO) {
+        var customerEntity = customerRepository.findById(customerId);
+
+        if (customerEntity.isPresent()) {
+            var customer = customerEntity.get();
+
+            if (updateCustomerDTO.phone() != null && !customer.getPhone().equals(updateCustomerDTO.phone())) {
+                boolean phoneExists = customerRepository.existsByPhone(updateCustomerDTO.phone());
+                if (phoneExists) {
+                    throw new IllegalArgumentException("Customer with given phone already exists");
+                }
+            }
+
+            if (updateCustomerDTO.name() != null) {
+                customer.setName(updateCustomerDTO.name());
+            }
+
+            if (updateCustomerDTO.phone() != null) {
+                customer.setPhone(updateCustomerDTO.phone());
+            }
+
+            customerRepository.save(customer);
+        } else {
+            throw new RuntimeException("Customer not found");
+        }
+    }
+
+    public void deleteCustomerById(String customerId) {
+        UUID id = UUID.fromString(customerId);
+        Optional<Customer> customerExists = customerRepository.findById(id);
+
+        if (customerExists.isPresent()) {
+            customerRepository.deleteById(id);
+        } else {
+            throw new RuntimeException("Customer not found");
+        }
     }
 }
