b985d4cefad64e75e8857e1335b61ab7f236cebc
berdnaski <berdnaski1177@gmail.com>
2024-09-06 20:12:53 -0300

    chore: cors configuration correct

--- src/main/java/github/com/berdnaski/sankhya_api/config/WebConfig.java
+++ src/main/java/github/com/berdnaski/sankhya_api/config/WebConfig.java
@@ -1,17 +1,15 @@
-import org.springframework.context.annotation.Bean;
+package github.com.berdnaski.sankhya_api.config;
+
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-
-    @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // URL do frontend React
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+        registry.addMapping("/**");
     }
 }
